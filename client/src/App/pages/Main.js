681f5ed4dafcdb90adfec3812f04a7c00403ba79
Tatsumi Suenaga <suenaga.1@osu.edu>
2019-04-09 01:15:36 -0400

    UI changes

--- client/src/App/pages/Main.js
+++ client/src/App/pages/Main.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Main extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { 
+      url: '',
+      search: ''
+      };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event){
+    
+  }
+
   render() {
     return (
       <div className="App">
           <h1>Url Web Scraper</h1>
           <Link to={'./resultTable'}>
+            <form>
+              <label>URL</label>
+              <input type="text" name="url" onChange={this.handleChange} />
+
+              <label>Search word</label>
+              <input type="text" name="search" onChange={this.handleChange} />
+            </form>
             <button variant="raised">
                 Search
             </button>
